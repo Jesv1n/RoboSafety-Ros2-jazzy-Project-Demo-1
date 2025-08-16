0e85056782761d5b8ce77a674e1dac30b1af57d6
Jesvin Paul <jesvinpaul18@gmail.com>
2025-08-16 09:03:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# ROS 2 fUNDAMENTALS EXAMPLES
+
+THIS PAVCKAGE CONTAINS EXAMPLESA DEMOSTRATING FUNDAMWENTAL ROS2 CONCEPT
+
+## Description
+THis package includes minimalist ros 2 code to demonstrated important ros 2 concepts and patterns
+
+- Following ros 2 python style guidelines
+
+
+## Prerequisties
+- Ros 2 installed
+- Python 3
+- Created a ros 2 workspace ('ros2_ws')
+
+## Author
+
+Jesvin paul
+
---
+++ ros2_fundamentals_examples/scripts/minimal_pub_sub_launch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cleanup(){
+    echo "Restarting ROS 2 daemon to cleanup before shutting down all processes...."
+    ros2 daemon stop
+    sleep 1
+    ros2 daemon start
+    echo "Terminating all ROS 2 related processes..."
+    kill 0
+    exit
+}
+
+trap 'cleanup' SIGINT
+
+# Launch publisher node in the background
+ros2 run ros2_fundamentals_examples py_minimal_publisher &
+
+# Give it a moment to start
+sleep 2
+
+# Launch subscriber node in the background
+ros2 run ros2_fundamentals_examples py_minimal_subscriber &
+
+# Wait for both processes to keep the script alive
+wait
